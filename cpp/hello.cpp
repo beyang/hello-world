9e26b9a0caf782355d46e3d0ba9bae86f2c4b90c
Beyang Liu <beyang@sourcegraph.com>
2020-11-03 16:28:22 -0800

    cpp: some vector stuff

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-  cout << "Hello world C++";
+  cout << "Hello world C++" << endl;
+  vector<int> v;
+  v.push_back(1);
+  v.push_back(2);
+  v.push_back(3);
+  for (vector<int>::iterator it = v.begin(), end = v.end(); it != end; ++it) {
+    cout << "Elem " << *it << endl;
+  }
   return 0;
 }
